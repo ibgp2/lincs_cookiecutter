3e50a8e3a2edb99b4a5f0259b1514bb4c7c13d80
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2022-01-19 12:47:13 +0100

    Bump version: 0.1.0 → 0.1.1

--- lincs_cookiecutter/__init__.py
+++ lincs_cookiecutter/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Marc-Olivier Buob"""
 __email__ = 'marc-olivier.buob@nokia-bell-labs.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 from lincs_cookiecutter.sub_package_1.my_class_1 import MyClass1
